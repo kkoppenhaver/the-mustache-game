2953054153965e6d9762b1d61c7938c6b7227652
Keanan Koppenhaver <k.koppenhaver@gmail.com>
2015-03-08 22:58:01 -0500

    displaying mustaches on page

--- extension.js
+++ extension.js
@@ -1,6 +1,39 @@
 function displayMustache(image_path) {
-	//Size the mustache somwehere between 10 and 20% of the window width
-	//Position it on the screen in a random position
+	var px_left = Math.round(Math.floor(Math.random() * ((w_width - width_padding) - width_padding)));
+	var px_top = Math.round(Math.floor(Math.random() * ((w_height - height_padding) - height_padding)));
+
+	//Place the image on the page
+	$('<img class="mustache">').attr('src', image_path)
+		.css('position', 'absolute')
+		.css('left', px_left)
+		.css('top', px_top)
+		.css('max-width',  '20%')
+		.appendTo('.mustache-container');
+		
 }
 
-//displayMustache('images/mustache1.svg');
+var mustaches = [
+	chrome.extension.getURL('images/mustache1.svg'),
+	chrome.extension.getURL('images/mustache2.svg'),
+	chrome.extension.getURL('images/mustache3.svg'),
+]
+
+$('.mustache').remove();
+var mustache_positions = [];
+
+var w_height = $(window).height();
+var w_width = $(window).width();
+var mustache_width = w_width * .2;
+var mustache_height = mustache_width / 2;
+
+
+var width_padding = w_width * .2;
+var height_padding = w_height * .2;
+
+$('<div class="mustache-container" style="position: absolute;z-index:10000;width:' +  w_width + 'px; height:' + w_height + 'px; top: 0; left: 0;">').appendTo('body');
+
+
+
+mustaches.forEach(function(mustache){
+	displayMustache(mustache);
+});
